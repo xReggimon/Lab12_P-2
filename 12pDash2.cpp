3af84513441427efe1f7fc7d316fa58856c707d2
xReggimon <r-mcmullen.1@onu.edu>
2024-04-25 19:23:03 -0400

    Copied code from Lab12_P-1

---
+++ 12pDash2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cctype>
+
+using namespace std;
+
+void pressEnterToContinue();
+
+
+int main() {
+
+
+	string datafile;
+	cout << "Enter filename: ";
+	getline(cin, datafile);
+	ifstream inFile(datafile);
+
+	string word;
+	int letters = 0;
+	int words = 0;
+
+	if (!inFile.is_open()) {
+		cerr << "Error opening file." << endl;
+		return 1;
+	}
+
+	while (inFile >> word) {
+		words++;
+		letters += word.size();
+	}
+
+	inFile.close();
+
+	cout << "Words: " << words << endl;
+	cout << "Letters: " << letters << endl;
+
+	pressEnterToContinue();
+	return 0;
+}
+
+
+void pressEnterToContinue() {
+	cout << "\nPress enter to continue... \n";
+	cin.clear();
+	while (cin.get() != '\n')continue;
+}
